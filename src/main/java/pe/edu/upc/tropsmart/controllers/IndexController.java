ae7e3a80c5e626ffbaaa7a0debedacb9a303c70e
c-jeff <powerjspxr@gmail.com>
2020-11-12 04:25:59 -0500

    Implemented HU01

--- src/main/java/pe/edu/upc/tropsmart/controllers/IndexController.java
+++ src/main/java/pe/edu/upc/tropsmart/controllers/IndexController.java
@@ -4,15 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import pe.edu.upc.tropsmart.models.entities.Transportista;
 
 @Controller
 @RequestMapping("/")
+@SessionAttributes("transportista")
 public class IndexController {
 	
 	@GetMapping
-	public String index(Model model) {
-		String saludo = "Hello Programmers";
-		model.addAttribute("saludo", saludo);
+	public String index(Model model) throws Exception {
+		Transportista transportista = new Transportista();
+		model.addAttribute("transportista", transportista);
 		return "index";
+	}
+	
+	@GetMapping("template")
+	public String template() {
+		return "template";
 	}
 }
