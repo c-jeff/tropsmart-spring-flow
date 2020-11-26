e20f33e42e6586b1646b665ca7caf58446d4a6b1
c-jeff <powerjspxr@gmail.com>
2020-11-26 01:32:17 -0500

    Implemented/Updated HU01-HU10

--- src/main/java/pe/edu/upc/tropsmart/models/repositories/DetalleServicioRepository.java
+++ src/main/java/pe/edu/upc/tropsmart/models/repositories/DetalleServicioRepository.java
@@ -1,12 +1,15 @@
 package pe.edu.upc.tropsmart.models.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.tropsmart.models.entities.DetalleServicio;
 
 @Repository
 public interface DetalleServicioRepository extends JpaRepository<DetalleServicio, Integer>{
-	
+	@Query("select d from DetalleServicio d where d.servicio.transportista.id = ?1 and d.servicio.estado <> ?2")
+	List<DetalleServicio> findByTransportistaAndNotEstado(Integer id, String estado) throws Exception;
 }
